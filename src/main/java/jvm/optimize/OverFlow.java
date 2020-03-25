ccf83f4187fab0e0955db52b80fb0e0b1c2a7859
gongzhenzhen <2206463420@qq.com>
2020-03-25 10:39:17 +0800

    方法区OOM场景

--- src/main/java/jvm/optimize/OverFlow.java
+++ src/main/java/jvm/optimize/OverFlow.java
@@ -1,5 +1,10 @@
 package jvm.optimize;
 
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,6 +45,34 @@ class StackOverFlow implements OverFlow{
     //循环调用方法不会出现，因为方法执行完释放，再执行再释放
     public static void main(String[] args) {
         OverFlow overFlow = new StackOverFlow();
+        overFlow.overFlow();
+    }
+}
+
+
+class MethodRegionOverFlow implements OverFlow{
+
+    //-XX:MetaspaceSize=10m -XX:MaxMetaspaceSize=10m 抛出OOM  创建了大量类型，方法区要存储大量动态创建加载的类型信息导致内存溢出 解决：关注运行时生成大量动态类的场景。
+    @Override
+    public void overFlow() {
+        while (true){
+            //cglib 生产大量动态类
+            Enhancer enhancer = new Enhancer();
+            enhancer.setSuperclass(MethodRegionOverFlow.class);
+            enhancer.setUseCache(false);
+            enhancer.setCallback(new MethodInterceptor() {
+                @Override
+                public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+                    return proxy.invoke(obj, args);
+                }
+            });
+            //动态生成新的代理类
+            enhancer.create();
+        }
+    }
+
+    public static void main(String[] args) {
+        OverFlow overFlow = new MethodRegionOverFlow();
         overFlow.overFlow();
     }
 }
