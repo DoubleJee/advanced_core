f6f88f841fd4e8c24fdcb78dc1c3820d14639167
gongzhenzhen <2206463420@qq.com>
2020-06-17 19:51:42 +0800

    红黑树 添加的三种情况平衡染色

--- src/main/java/datastructure/tree/binary/RBTree.java
+++ src/main/java/datastructure/tree/binary/RBTree.java
@@ -55,7 +55,7 @@ public class RBTree<E extends Comparable> extends BinarySearchTree<E>{
 
 
     class RBNode<E> extends Node<E>{
-        private boolean color;
+        private boolean color = RED;
         public RBNode(E element, Node<E> parent) {
             super(element, parent);
         }
