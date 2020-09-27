31a452968c3b09c3fd83c6ee1b0f355ba1d74788
gongzhenzhen <2206463420@qq.com>
2020-09-27 13:20:17 +0800

    数据结构——HashTab哈希表之 HashMap，hash取模优化、equals方法和hashCode方法规范、Hash表时间复杂度、HashMap和TreeMap性能对比

--- src/main/java/datastructure/hash/PerfTest.java
+++ src/main/java/datastructure/hash/PerfTest.java
@@ -2,6 +2,7 @@ package datastructure.hash;
 
 import datastructure.TimeTool;
 import datastructure.other.map.Map;
+import datastructure.other.map.TreeMap;
 
 public class PerfTest {
 
@@ -10,6 +11,21 @@ public class PerfTest {
 
         TimeTool.checkTime("HashMap", () -> {
             Map<Integer, Integer> map = new HashMap<>();
+            for (int i = 0; i < 100000; i++) {
+                map.put(i, i);
+            }
+
+            for (int i = 0; i < 100000; i++) {
+                map.get(i);
+            }
+
+            for (int i = 0; i < 100000; i++) {
+                map.remove(i);
+            }
+        });
+
+        TimeTool.checkTime("TreeMap", () -> {
+            Map<Integer, Integer> map = new TreeMap<>();
             for (int i = 0; i < 100000; i++) {
                 map.put(i, i);
             }
